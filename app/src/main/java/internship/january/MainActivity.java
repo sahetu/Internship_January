00c03971607106dd578351adcd1e4ec8ad8177c2
Sagar Khatri <khatrisagar2@gmail.com>
2025-01-07 11:53:39 +0530

    Login Form

--- app/src/main/java/internship/january/MainActivity.java
+++ app/src/main/java/internship/january/MainActivity.java
@@ -1,18 +1,48 @@
 package internship.january;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button login;
+    EditText username,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.main_login);
+        username = findViewById(R.id.main_username);
+        password = findViewById(R.id.main_password);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(username.getText().toString().trim().equals("")){
+                    username.setError("Username Required");
+                }
+                else if(password.getText().toString().trim().equals("")){
+                    password.setError("Password Required");
+                }
+                else if(password.getText().toString().trim().length()<6){
+                    password.setError("Min. 6 Char Password Required");
+                }
+                else{
+                    System.out.println("Login Successfully");
+                    Toast.makeText(MainActivity.this, "Login Successfully", Toast.LENGTH_LONG).show();
+                    Snackbar.make(view,"Login Successfully",Snackbar.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
